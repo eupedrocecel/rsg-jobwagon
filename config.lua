98a17d271f318fe31d95d61ec7d4a5468bbad033
RexShack <rexshackuk@gmail.com>
2022-12-31 19:37:01 +0000

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,9 +1,45 @@
 Config = {}
 
 -- wholesale trader settings
-Config.WholesaleTrader = 'CART01'
+Config.WholesaleTrader = 'wagon04x'
 Config.WholesaleTraderSpawn = vector4(2333.1894, -1479.587, 45.956836, 148.38502)
 
 -- settings
 Config.MaxWeight = 500000
 Config.MaxSlots = 40
+
+--[[ cart information
+wagontraveller01x
+wagon06x
+wagon05x
+wagon04x
+wagon03x
+wagon02x
+UTILLIWAG
+supplywagon
+stagecoach006x
+stagecoach005x
+stagecoach004x
+stagecoach002x
+stagecoach001x
+logwagon
+hutercart01
+coach6
+coach5
+coach4
+coach3
+coach2
+chuckwagon002x
+chuckwagon000x
+cart08
+cart07
+cart06
+cart05
+cart04
+cart03
+cart02
+cart01
+buggy03
+buggy02
+buggy01
+--]]
